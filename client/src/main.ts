0f8a9b4f8d5aa6718eafa9842d0aa53eef9aa59d
camslaw <ccestridge8@gmail.com>
2026-01-30 00:37:42 -0500

    Remove unnecessary debug components from ui and adjust canvas

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,16 +1,21 @@
 import Phaser from "phaser";
 import BootScene from "./scenes/BootScene";
-import MenuScene from "./scenes/MenuScene";   // now Auth
-import MatchScene from "./scenes/MatchScene"; // new
+import MenuScene from "./scenes/MenuScene";
+import MatchScene from "./scenes/MatchScene";
 import LobbyScene from "./scenes/LobbyScene";
 import ArenaScene from "./scenes/ArenaScene";
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
-  width: 1200,
-  height: 720,
-  backgroundColor: "#1d1f27",
   parent: "app",
+  backgroundColor: "#1d1f27",
+
+  scale: {
+    mode: Phaser.Scale.RESIZE,
+    width: window.innerWidth,
+    height: window.innerHeight,
+  },
+
   scene: [BootScene, MenuScene, MatchScene, LobbyScene, ArenaScene],
   dom: { createContainer: true },
   pixelArt: true,
